42a40524adfd2a48afc2751317a792b595c1a05f
MohammedNayeem9 <96781259+MohammedNayeem9@users.noreply.github.com>
2023-04-09 16:42:01 +0530

    Update style.js

--- style.js
+++ style.js
@@ -1,1 +1,13 @@
 
+
+
+// function gotolink(link){
+//     console.log(link.value);
+//     //location.href = link.value;
+//     window.open(link.value);
+// };
+
+
+function redirectToPage(url) {
+    window.open(url, '_blank');
+  }
